2f50ece544501030ef6aef8cb8c7021f2f66148c
Ezreal Song <ssong56@asu.edu>
2022-04-19 16:16:25 -0700

    IdleMoveAnimatorDone

--- Final Project/Assets/Scripts/Enemy.cs
+++ Final Project/Assets/Scripts/Enemy.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
     private Transform player;
+    [SerializeField]
     private float distance;
+    [SerializeField]
     public float moveSpeed;
-    public float close;
+    [SerializeField]
+    public float FindingRange;
+    [SerializeField]
+    NavMeshAgent Skeleton;
+    [SerializeField]
+    private Animator Animator;
+    [SerializeField]
+
+    private bool canMove = false;
+    private bool canAttack = false;
+  
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        Skeleton = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -19,10 +34,24 @@ public class Enemy : MonoBehaviour
     {
         distance = Vector3.Distance(player.position, transform.position);
 
-        if(distance <= close)
+        if(distance <= FindingRange)
         {
-            transform.LookAt(player);
-            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
+            canMove = true;
+            //transform.LookAt(player);
+            //GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
+            if(canMove == true)//player in range
+            {
+                Animator.SetBool("PlayerFound", true);
+                Skeleton.SetDestination(player.transform.position);
+
+            }
+            
+        }
+        else //player out of range
+        {
+            canMove = false;
+            Animator.SetBool("PlayerFound", false);
+            Skeleton.SetDestination(this.transform.position);
         }
     }
 }
