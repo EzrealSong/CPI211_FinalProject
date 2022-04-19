07e5c04b9d32548b3a7754356914ef3f55a18350
WaffleChomper <92765026+WaffleChomper@users.noreply.github.com>
2022-04-19 13:56:18 -0700

    Changes for Check in 2
    Gun Animations for scoping in (right click)
    Camera Shake
    Sniper Scope has been created.
    
    To-do: Interact element

--- Final Project/Assets/Scripts/WeaponSwitching.cs
+++ Final Project/Assets/Scripts/WeaponSwitching.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class WeaponSwitching : MonoBehaviour
 {
 
-    public int selectedWeapon = 0;
+    public static int selectedWeapon = 0;
     // Start is called before the first frame update
     void Start()
     {
