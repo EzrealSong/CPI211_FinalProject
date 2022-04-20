eee5e723699424fa35f7e51d14a3e04674a0d1c5
Ezreal Song <ssong56@asu.edu>
2022-04-19 17:33:32 -0700

    Basic animation done

--- Final Project/Assets/Scripts/Target.cs
+++ Final Project/Assets/Scripts/Target.cs
@@ -5,18 +5,24 @@ public class Target : MonoBehaviour
 {
     // Start is called before the first frame update
     public float health = 50f;
+    public bool objectdead = false;
+    private float time = 2.5f;
 
     public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0f)
         {
+            objectdead = true;
             Die();
         }
     }
     void Die()
     {
-        Destroy(gameObject);
+        //play death animation
+        this.gameObject.GetComponent<Animator>().Play("Death");
+       
+       Destroy(gameObject,time);
     }
 
 }
