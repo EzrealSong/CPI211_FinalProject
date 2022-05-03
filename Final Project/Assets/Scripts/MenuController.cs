7980132439a4fc47b0278a01a52000d10f14d4f3
Ezreal Song <ssong56@asu.edu>
2022-05-03 15:19:56 -0700

    voice setting is available now

--- Final Project/Assets/Scripts/MenuController.cs
+++ Final Project/Assets/Scripts/MenuController.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
 
-   [SerializeField] public Animator mainMenuAnim;
+    [Header("Volume Setting")]
+    [SerializeField] private Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private int defaultVolume = 10;
+    [SerializeField] public Animator mainMenuAnim;
     //Play Game
     public void PlayGame()
     {
@@ -26,4 +31,30 @@ public class MenuController : MonoBehaviour
         mainMenuAnim.SetBool("IsFadeOut", true);
         yield return new WaitForSeconds(5);
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0");
+            VolumeApply();
+        }
+    }
+
+    
+
 }
